7be85b333df34e5b82396254e5dd609b9a11be79
ipdragon1537 <ipdragon1537@gmail.com>
2026-07-18 00:20:51 +0700

    Upload ss19/schemas.py

---
+++ ss19/schemas.py
@@ -0,0 +1,59 @@
+from typing import Optional, List
+from pydantic import BaseModel
+class WaybillCreate(BaseModel):
+    tracking_number: str
+    shipping_status: str
+    package_id: int
+
+
+class WaybillResponse(BaseModel):
+    id: int
+    tracking_number: str
+    shipping_status: str
+    package_id: int
+
+    class Config:
+        from_attributes = True
+class PackageCreate(BaseModel):
+    package_code: str
+    weight: float
+    warehouse_id: int
+
+
+class PackageUpdate(BaseModel):
+    package_code: Optional[str] = None
+    weight: Optional[float] = None
+    warehouse_id: Optional[int] = None
+
+
+class PackageResponse(BaseModel):
+    id: int
+    package_code: str
+    weight: float
+    warehouse_id: int
+    waybill: Optional[WaybillResponse] = None
+
+    class Config:
+        from_attributes = True
+class WarehouseCreate(BaseModel):
+    warehouse_name: str
+    location: str
+
+
+class WarehouseResponse(BaseModel):
+    id: int
+    warehouse_name: str
+    location: str
+
+    class Config:
+        from_attributes = True
+
+
+class WarehouseDetailResponse(BaseModel):
+    id: int
+    warehouse_name: str
+    location: str
+    packages: List[PackageResponse] = []
+
+    class Config:
+        from_attributes = True
